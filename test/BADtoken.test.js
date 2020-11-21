78204de77e4e12da56fad33d271d0e0b410f691f
Sven Meyer <git_dev@svenmeyer.com>
2020-11-21 21:35:23 +1100

    7 test cases passing

--- test/BADtoken.test.js
+++ test/BADtoken.test.js
@@ -10,17 +10,61 @@ const BADtoken = artifacts.require('BADtoken');
 
 // Start test block
 contract('BADtoken', function ([ owner, other ]) {
-  beforeEach(async function () {
-    // Deploy a new Box contract for each test
+  // Deploy a new contract
+  before(async function () {
     this.token = await BADtoken.new();
   });
 
-  // Test case
+  const decimalZeros = '000000000000000000'; // 18 x '0'
+
+  // Test case 1
+  it('totalSupply should be 10,000,000 token', async function () {
+    expect((await this.token.totalSupply()).toString()).to.equal('10'+'000'+'000'+decimalZeros);
+  });
+
+  // Test case 2
+  it('owner should have 10,000,000 token', async function () {
+    expect((await this.token.balanceOf(owner)).toString()).to.equal('10'+'000'+'000'+decimalZeros);
+  });
+
+  // Test case 3
+  it('account owner can burn his token', async function () {
+    const burnAmount = new BN('2'+'000'+'000'+decimalZeros);
+    const balance = this.token.balanceOf(owner);
+    this.token.burn(burnAmount);
+    expect((await this.token.balanceOf(owner)).toString()).to.equal('8'+'000'+'000'+decimalZeros);
+  });
+
+  // Test case 4
   it('no account should be blacklisted at the start', async function () {
     expect((await this.token.isBlackListed( other )).toString()).to.equal('false');
   });
 
-//    isBlackListed(accounts[1])
+  // Test case 5
+  it('owner can transfer token to other account', async function () {
+    const amount = new BN('3'+'000'+'000'+decimalZeros);
+    const result = await this.token.transfer(other, amount, { from: owner });
+    expect(result.receipt.status.toString()).to.equal('true');
+    expect((await this.token.balanceOf(other)).toString()).to.equal('3'+'000'+'000'+decimalZeros);
+  });
+
+  // Test case 6
+  it('blacklist other account', async function () {
+    await this.token.blacklistUpdate(other, true);
+    expect((await this.token.isBlackListed(other)).toString()).to.equal('true');
+  });
+
+  // Test case 7
+  it('transfer token to blacklisted other account should fail', async function () {
+    const amount = new BN('3'+'000'+'000'+decimalZeros);
+
+    await expectRevert(
+      this.token.transfer(other, amount, { from: owner }),
+      "Token transfer refused. Recipient is on blacklist"
+    );
+
+    expect((await this.token.balanceOf(other)).toString()).to.equal('3'+'000'+'000'+decimalZeros);
+  });
 
 
 });
