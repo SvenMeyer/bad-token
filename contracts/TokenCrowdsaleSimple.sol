04c32da9b0a12129fc2d7cf0e358017a2467cd46
Sven Meyer <git_dev@svenmeyer.com>
2020-11-23 23:37:12 +1100

    add Token Crowdsale contract

---
+++ contracts/TokenCrowdsaleSimple.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.5.17;
+
+// https://docs.openzeppelin.com/contracts/2.x/crowdsales
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.1/contracts/crowdsale/Crowdsale.sol";
+
+/*
+	constructor / deployment parameter
+	rate   : number (uint256) of fractional token per wei
+	wallet : receiver (address) of ETH funds
+	token  : token (address) to be solf for ETH
+
+	function buyTokens(address beneficiary)
+	ETH amount sent with transaction will be converted of token amount according to rate
+	token will be send to provided beneficiary (address)
+*/
+
+contract TokenCrowdsale is Crowdsale {
+    constructor(
+        uint256 rate,
+        address payable wallet,
+        IERC20 token
+    )
+        Crowdsale(rate, wallet, token)
+        public
+    {
+
+    }
+
+    function buyTokens() public nonReentrant payable {
+        buyTokens(msg.sender);
+    }
+}
